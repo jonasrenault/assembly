9e4f5816ba2b47449ac69d1131be13193f2fedd6
Jonas Renault <jonas.renault@inria.fr>
2017-07-26 10:13:08 +0200

    added deputies count

--- src/Assembly/Assembly.js
+++ src/Assembly/Assembly.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
 import Seat from '../Seat/Seat';
 import { ordrepartis, sexorder } from '../constants/constants';
+import './Assembly.css';
+
 
 class Assembly extends PureComponent {
   render() {
@@ -9,13 +11,18 @@ class Assembly extends PureComponent {
       return ordrepartis[a.etiquette_pandore] - ordrepartis[b.etiquette_pandore] || sexorder[a.sexe] - sexorder[b.sexe];
     });
     const seats = m.length;
+    const active = m.filter(elt => elt.active).length;
 
     const circles = m.map((elt, idx) => <Seat key={elt.id} seat={elt} idx={idx} seats={seats} layout={this.props.layout} onSelectDeputy={this.props.onSelectDeputy}></Seat>);
-
+    const ww = this.props.layout.width - this.props.layout.margin.left;
+    const hh = this.props.layout.height - this.props.layout.margin.bottom;
     return (
       <svg width={this.props.layout.width} viewBox={viewBox}>
         <g transform={'translate(' + (this.props.layout.width / 2) + ',' + (this.props.layout.height - this.props.layout.margin.bottom) + ') rotate(180)'}>
           {circles}
+        </g>
+        <g>
+          <text className="assembly-count" textAnchor="middle" x={(ww / 2)} y={hh}>{active} député{active > 1 ? 's' : ''}</text>
         </g>
       </svg>
     );
